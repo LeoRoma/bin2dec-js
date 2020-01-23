b4b234fc6496dcaba04cb5ff36a93c9e00441938
LeoRoma <xiajtn@gmail.com>
2020-01-23 20:36:50 +0000

    possible to get input from user

--- interface.js
+++ interface.js
@@ -4,19 +4,21 @@ document.addEventListener("DOMContentLoaded", function(event) {
   document.getElementById("submit").addEventListener('click', function(event) {
     event.preventDefault();
     // get text input, create note and add to list
-    var binary = document.getElementById("binary")
-    binary = binary.value;
+    let getBinary = document.getElementById("binary")
+    let binary = getBinary.value;
     
-    binaryconverter.input(binary);
+    console.log("hello")
+    console.log(binary)
+    // binaryconverter.input(binary);
 
-    // create new `li` HTMLElement and insert
-    // innerHTML with note content and id
-    // if you want a link instead of heading, change h2 to <a>
-    var li = document.createElement("li");
-    li.innerHTML = "<h2>" + binaryconverter.decimal + "</h2>";
+    // // create new `li` HTMLElement and insert
+    // // innerHTML with note content and id
+    // // if you want a link instead of heading, change h2 to <a>
+    // var li = document.createElement("li");
+    // li.innerHTML = "<h2>" + binaryconverter.decimal + "</h2>";
 
-    var ul = document.getElement("result-decimal");
-    ul.appendChild(li)
+    // var ul = document.getElement("result-decimal");
+    // ul.appendChild(li)
     
    
 
